a1c17f2b731106dd5e7912a161bd292c762c1622
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-11 14:51:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,5 +25,12 @@ class Dog
       DB[:conn].execute("DROP TABLE dogs")
   end
 
+  def save 
+    sql = <<-SQL 
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+  end
+
 
 end
